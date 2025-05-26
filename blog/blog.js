c1f1e5bb3923e03fbe4429925e19d4dff3798086
Lora Chisholm <chi20006@byui.edu>
2025-05-26 16:07:40 -0600

    blog build

---
+++ blog/blog.js
@@ -0,0 +1,19 @@
+// book.js
+//contains data for two books to be used in the blog layout later
+
+const books = [
+    {
+        title: "The Great Gatsy",
+        author: "F. Scott Fitzgerald",
+        date: "1925",
+        image: "images/gatsby.jpg",
+        description: " A novel set in the Jazz Age that tells the story of Jay Gatsby's tragic pursuit of wealth and romance. "
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper, Lee",
+        date: "1960",
+        image: "images/mockingbird.jpg", 
+        description: "A story of racial injustice and moral growth in the deep south through the eyes of a young girl"
+    }
+];
