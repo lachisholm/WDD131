961991e0efdc44d7956721202c01f6bb19263a96
Lora Chisholm <chi20006@byui.edu>
2025-05-26 20:40:10 -0600

    fix js

--- javascript_debugging/debugging.js
+++ javascript_debugging/debugging.js
@@ -1,8 +1,10 @@
 const PI = 3.14;
 const radius = 3;
 let area = 0;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area1:", area);
 radius = 4;
-area = radius * radius * pi;
+area = radius * radius * PI;
+console.log("Area2:", area);
 
 /*console.log("Javascript is connected")*/
