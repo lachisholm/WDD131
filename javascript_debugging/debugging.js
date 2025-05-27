59fac61b4da9cfde2eabc97a9591dfa4bc4a328b
Lora Chisholm <chi20006@byui.edu>
2025-05-26 22:27:07 -0600

    fix js files

--- javascript_debugging/debugging.js
+++ javascript_debugging/debugging.js
@@ -1,8 +1,12 @@
 const PI = 3.14;
 const radius = 3;
 let area = 0;
+
+
 area = radius * radius * PI;
 console.log("Area1:", area);
+
+
 radius = 4;
 area = radius * radius * PI;
 console.log("Area2:", area);
