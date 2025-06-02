1baeaa0232c03ea25392ff46cd5a2aa9ab9b8c99
Lora Chisholm <chi20006@byui.edu>
2025-06-02 16:40:36 -0600

    add js

---
+++ blog_2/blog_2.js
@@ -0,0 +1,43 @@
+// book.js
+//contains data for two books to be used in the blog layout later
+
+const books = [
+    {
+        title: "The Great Gatsy",
+        author: "F. Scott Fitzgerald",
+        date: "1925",
+        image: "images/gatsby.jpg",
+        description: " A novel set in the Jazz Age that tells the story of Jay Gatsby's tragic pursuit of wealth and romance. "
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper, Lee",
+        date: "1960",
+        image: "images/mockingbird.jpg", 
+        description: "A story of racial injustice and moral growth in the deep south through the eyes of a young girl"
+    }
+];
+
+// grab the container where articles should go
+const container = document.guerySelector('.articles');
+
+//For each book object, create and append an <article>;
+books.forEach(book => {
+    const articleE1 = document.createElement('article');
+    articleE1.classList.add(''); //add any needed classes, or leave blank)
+
+
+   articleEl.innerHTML = `
+    <div class="article-details">
+      <p class="author">By: ${book.author}</p>
+      <time class="date" datetime="${book.date}">${book.date}</time>
+    </div>
+    <div class="article-content">
+      <h2>${book.title}</h2>
+      <img src="${book.image}" alt="Cover of ${book.title}">
+      <p>${book.description}</p>
+    </div>
+  `;
+
+  container.appendChild(articleE1);
+});
