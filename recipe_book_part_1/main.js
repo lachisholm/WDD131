d3c0a7a7d0de1babc70c7cb5c66748d61edb2c75
Lora Chisholm <chi20006@byui.edu>
2025-06-09 15:35:53 -0600

    added mjs and js and fixed the main

---
+++ recipe_book_part_1/main.js
@@ -0,0 +1,9 @@
+// main.js
+import { recipes } from './recipes.mjs';
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Main JS loaded');
+        const listcontainer = document.querySelector('.recipes-list')
+        recipes.forEach(recipe => {
+        });
+    });
---
+++ recipe_book_part_1/recipes.mjs
@@ -0,0 +1,17 @@
+// recipes.mjs
+
+export const recipes = [
+    { id: 1, 
+      name: "Chocolate Cake", 
+      image: "images/chocolate-cake.jpg", 
+      description: " A rich and moist chocolate cake perfect for dessert", 
+      rating: 4 
+    },
+    {
+        id: 2,
+        name: "Avocado Toast",
+        image: "images/avocado-toast.jpg",
+        description: "Creamy avocado spread on toasted bread, topped with seasoning.",
+        rating: 5 },
+
+];
