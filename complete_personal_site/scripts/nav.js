29e11ea13cf7e09eed96adaad99f7fbd3a8b5b9b
Lora Chisholm <chi20006@byui.edu>
2025-06-24 19:30:07 -0600

    js .main-nav

--- complete_personal_site/scripts/nav.js
+++ complete_personal_site/scripts/nav.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const hamburger = document.querySelector (".hamburger");
-    const nav = document.querySelector("nav");
+    const nav = document.querySelector(".main-nav");
 
     if (hamburger && nav) {
         hamburger.addEventListener("Click", () => {
