1bf5c70ded814ad24a0b6009c61c81eae568f490
Lora Chisholm <chi20006@byui.edu>
2025-06-30 22:36:27 -0600

    add js file

---
+++ teddy_bears/main.js
@@ -0,0 +1,46 @@
+// Select the hamburger icon by its class name and store it in a variable
+const hamburger = document.querySelector('.hamburger')
+
+// Select the nav-links list by its class name and store it in a variable.
+const navLinks = document.querySelector('.nav-links');
+
+// Add a click event listener to the hamburger icon
+hamburger.addEventListener('click', () => {
+    
+//Toggle the 'active' class on the nav0links element
+navLinks.classList.toggle('active');
+
+//If nav links now has the 'active class, log 'Menu open'
+if (navLinks.classList.contains('active')) {
+    console.log('Menu opened');
+} else {
+    console.log('Menu closed');
+}
+});
+
+
+const teddyBear = {
+    name: 'Classic Brown Teddy',
+    size: 'Medium',
+    price: 19.99,
+    inStock: true
+};
+
+// Log the teddy bear object to the console
+console.log('Teddy Bear Object:', teddyBear);
+
+// teddy bear names
+const teddyBearNames = ['Brownie','Snowflake', 'Honey', 'Cuddles'];
+
+
+//array method to log each teddy bear name
+teddyBearNames.forEach(function(name) {
+    console.log('Teddy Bear Name:', name);
+});
+
+if (teddyBear.instock) {
+    console.log(teddyBear.name + 'is available for purchase.');
+}else{
+    console.log(teddyBear.name + 'is out of stock.');
+}
+
